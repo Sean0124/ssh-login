973fa0ea656c90ea731c46988285f720074d9cf2
sean <sean.x.dbm@gmail.com>
2024-12-14 18:39:56 +0800

    add READEME

--- core.ex
+++ core.ex
@@ -1,17 +1,34 @@
-#!/usr/bin/expect
-set ip [lindex $argv 0]
-set port [lindex $argv 1]
-set username [lindex $argv 2]
-set password [lindex $argv 3]
-#set timeout -1
-spawn ssh -p $port $username@$ip
-expect {
-	"password" {send "$password\r";}
-	"yes/no" {send "yes\r";exp_continue}
-}
-expect "$username" {send "sudo -i\r"}
-expect {
-	"password" {send "$password\r";}
-	"yes/no" {send "yes\r";exp_continue}
-}
+#!/usr/bin/expect  
+
+# 获取命令行参数  
+set ip [lindex $argv 0]  
+set port [lindex $argv 1]  
+set username [lindex $argv 2]  
+set password [lindex $argv 3]  
+set use_sudo [lindex $argv 4]  ; # 新增参数，控制是否使用 sudo  
+
+# 设置超时时间  
+set timeout 20  
+
+# 启动 SSH 连接  
+spawn ssh -p $port $username@$ip  
+
+# 处理密码提示  
+expect {  
+    "password:" { send "$password\r" }  
+    "yes/no" { send "yes\r"; exp_continue }  
+}  
+
+# 如果需要使用 sudo，则执行 sudo -i  
+if {$use_sudo == "true"} {  
+    expect "$username" {  
+        send "sudo -i\r"  
+    }  
+    expect {  
+        "password for $username:" { send "$password\r" }  
+        "yes/no" { send "yes\r"; exp_continue }  
+    }  
+}  
+
+# 进入交互模式  
 interact
--- login
+++ login
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# 获取是否使用 sudo 的参数
+use_sudo="${1:-n}"  # 默认值为 'n'
+
 dir="${HOME}/ssh-login-tool/"
 file="${dir}computerInfo"
 echo $file
@@ -7,6 +11,20 @@ echo $file
 awk '{if ($1 !~ /^#/ && NR > 1 && $1 != ""){printf "%-2s %-45s %-15s \n",NR")",$5,$1}}' $file 
 echo "please choose which machine to login:"
 read number
+
+# 验证输入是否为有效数字
+if ! [[ "$number" =~ ^[0-9]+$ ]]; then
+    echo "无效输入，请输入一个数字。"
+    exit 1
+fi
+
 #将信息存入变量
 read ip port user password <<< $(echo `awk 'NR=="'$number'"{print $1,$2,$3,$4}' $file`)
-expect ${dir}core.ex $ip $port $user $password
+
+# 调用 Expect 脚本
+sudo="false"
+if [ "$use_sudo" == "y" ]; then
+    sudo="true"
+fi
+#expect "${dir}core.ex" "$ip" "$port" "$user" "$password" "$sudo"
+expect "core.ex" "$ip" "$port" "$user" "$password" "$sudo"
